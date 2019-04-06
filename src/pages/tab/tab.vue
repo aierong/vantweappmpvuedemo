6ac60738243c4314c41ff49b2b15b4b5c7fe2044
aierong <aierong@126.com>
2019-04-06 12:06:43 +0800

    =

---
+++ src/pages/tab/tab.vue
@@ -0,0 +1,50 @@
+<!--
+作者:chenghao
+功能:vue页
+-->
+
+<!-- html代码片段 -->
+<template>
+
+  <div>
+
+  </div>
+
+</template>
+
+<!-- js脚本代码片段 -->
+<script>
+  export default {
+    name : "tab" ,
+    //数据模型
+    data () {
+      return {
+        msg : ''
+      }
+    } ,
+    //方法
+    methods : {
+      //methodsname() {
+      //代码搞这里
+      //},
+
+    } ,
+    //计算属性
+    computed : {
+      //name() {
+      //代码搞这里
+      //return this.data;
+      //}
+    } ,
+    //生命周期(mounted)
+    mounted () {
+
+    } ,
+  }
+</script>
+
+<!-- 样式代码片段  scoped -->
+<style scoped
+       src="./tab.css">
+
+</style>
