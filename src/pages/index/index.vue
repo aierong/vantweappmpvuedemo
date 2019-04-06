a6d5438b0ae5b50e5763d10e0acf6d6ede6a4304
aierong <aierong@126.com>
2019-04-06 12:23:00 +0800

    1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,35 +1,41 @@
 <template>
   <div>
 
+    <div v-for="(item,index) in mydemolist"
+         :key="index">
+      <van-cell :title="item.txt"
+                icon="location-o"
+                @click="onclick(item.pageurl)"
+                is-link/>
+    </div>
+    <!--    <van-cell title="按钮"-->
+    <!--              icon="location-o"-->
+    <!--              @click="buttonclick"-->
+    <!--              is-link/>-->
+    <!--    <van-cell-->
+    <!--      title="单元格"-->
+    <!--      value="内容"-->
+    <!--      size="large"-->
+    <!--      label="描述信息"-->
+    <!--    />-->
 
-    <van-cell title="按钮"
-              icon="location-o"
-              @click="buttonclick"
-              is-link/>
-    <van-cell
-      title="单元格"
-      value="内容"
-      size="large"
-      label="描述信息"
-    />
+    <!--    <van-cell-->
+    <!--      title="单元格"-->
+    <!--      icon="location-o"-->
+    <!--    />-->
 
-    <van-cell
-      title="单元格"
-      icon="location-o"
-    />
-
-    <van-cell-group>
-      <van-cell
-        title="单元格"
-        value="内容"
-      />
-      <van-cell
-        title="单元格"
-        value="内容"
-        label="描述信息"
-        border="false"
-      />
-    </van-cell-group>
+    <!--    <van-cell-group>-->
+    <!--      <van-cell-->
+    <!--        title="单元格"-->
+    <!--        value="内容"-->
+    <!--      />-->
+    <!--      <van-cell-->
+    <!--        title="单元格"-->
+    <!--        value="内容"-->
+    <!--        label="描述信息"-->
+    <!--        border="false"-->
+    <!--      />-->
+    <!--    </van-cell-group>-->
   </div>
 </template>
 
@@ -38,12 +44,27 @@
   export default {
     data () {
       return {
-        motto : '' ,
+        mydemolist : [
+          {
+            txt : '按钮' ,
+            pageurl : 'button'
+          } ,
+
+          {
+            txt : 'tab' ,
+            pageurl : 'tab'
+          }
+
+        ] ,
 
       }
     } ,
 
     methods : {
+      onclick ( pageurl ) {
+        const url = `../${ pageurl }/main`
+        wx.navigateTo( { url : url } )
+      } ,
       buttonclick () {
         const url = "../button/main"
         wx.navigateTo( { url : url } )
--- src/pages/tab/tab.vue
+++ src/pages/tab/tab.vue
@@ -7,7 +7,27 @@
 <template>
 
   <div>
-
+    <van-tabs :active="active"
+              @change="onChange">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+    <br><br><br>
+    <!--    多于 4 个标签时，Tab 可以横向滚动-->
+    <div>Tab可横向滚动</div>
+    <van-tabs :active=" active ">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+      <van-tab title="标签 6">内容 6</van-tab>
+      <van-tab title="标签7">内容7</van-tab>
+      <van-tab title="标签8">内容8</van-tab>
+      <van-tab title="标签9">内容9</van-tab>
+    </van-tabs>
   </div>
 
 </template>
@@ -19,14 +39,19 @@
     //数据模型
     data () {
       return {
-        msg : ''
+        //从0开始的
+        active : 2
       }
     } ,
     //方法
     methods : {
-      //methodsname() {
-      //代码搞这里
-      //},
+      onChange ( event ) {
+        //代码搞这里
+        console.log( event )
+
+        //选择的索引
+        console.log( event.mp.detail.index )
+      } ,
 
     } ,
     //计算属性
