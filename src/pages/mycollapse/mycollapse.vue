1f4a9b760222955e6079cd9df904a96fe163f01b
aierong <aierong@126.com>
2019-04-07 17:23:36 +0800

    =

--- src/pages/mycollapse/mycollapse.vue
+++ src/pages/mycollapse/mycollapse.vue
@@ -3,10 +3,14 @@
 功能:vue页
 -->
 
-<!-- html代码片段 -->
+ 
 <template>
-
+  <!--
+  特别提示:
+  van-collapse组件的:value="activeNames"不是双向数据绑定, 所以我们需要在事件change中主动赋值一下,这样才会有效果
+  -->
   <div>
+
     <van-collapse :value="activeNames"
                   @change="onChange">
       <van-collapse-item title="有赞微商城"
@@ -24,18 +28,46 @@
         线上拓客，随时预约，贴心顺手的开单收银
       </van-collapse-item>
     </van-collapse>
+
+    <mybr/>
+    <mybr/>
+    <div>手风琴效果</div>
+    <van-collapse :value="activeName2"
+                  @change="onChange2"
+                  accordion>
+      <van-collapse-item title="有赞微商城"
+                         name="1">
+        提供多样店铺模板，快速搭建网上商城
+      </van-collapse-item>
+      <van-collapse-item title="有赞零售"
+                         name="2">
+        网店吸粉获客、会员分层营销、一机多种收款，告别经营低效和客户流失
+      </van-collapse-item>
+      <van-collapse-item title="有赞美业"
+                         name="3">
+        线上拓客，随时预约，贴心顺手的开单收银
+      </van-collapse-item>
+    </van-collapse>
   </div>
 
 </template>
 
 <!-- js脚本代码片段 -->
 <script>
+  import mybr from '@/components/mybr/mybr.vue'
+
   export default {
     name : "mycollapse" ,
+    components : {
+      mybr
+    } ,
     //数据模型
     data () {
       return {
-        activeNames : [ "1" ]
+        //普通版本是数组
+        activeNames : [ "1" ] ,
+        //手风琴的是一个值
+        activeName2 : '1'
       }
     } ,
     //方法
@@ -45,10 +77,15 @@
 
         console.log( event.mp.detail )
 
-        //由于不是:value="activeNames" 不是双向数据绑定 这里我们要主动赋值一下
+        //由于 :value="activeNames" 不是双向数据绑定 这里我们要主动赋值一下
         this.activeNames = event.mp.detail;
       } ,
+      onChange2 ( event ) {
+        console.log( event )
 
+        //由于 :value="activeName2" 不是双向数据绑定 这里我们要主动赋值一下
+        this.activeName2 = event.mp.detail;
+      } ,
     } ,
     //计算属性
     computed : {
