a101b5ed61c62270e114725a5818b86c92c54233
aierong <aierong@126.com>
2019-04-10 13:55:44 +0800

    ==

--- CodeDemo/README.md
+++ CodeDemo/README.md
@@ -8,6 +8,17 @@ https://github.com/zxz-googleman/hello-mpvue-vantweapp
 
 
 
+1.npm 报错 Module build failed: Error: No PostCSS Config found in
+解决办法:
+需要在根文件里面新建一个文件postcss.config.js
+
+参考:https://blog.csdn.net/merciwen/article/details/79663569
+
+
+
+2.小程序开发工具打开后，首页报错
+解决办法:
+
 
 
 
--- CodeDemo/code/hellompvuevantweapp/src/App.vue
+++ CodeDemo/code/hellompvuevantweapp/src/App.vue
@@ -21,11 +21,11 @@ export default {
   padding: 200rpx 0;
   box-sizing: border-box;
 }
-/* this rule will be remove */
+/* this rule will be remove
 * {
   transition: width 2s;
   -moz-transition: width 2s;
   -webkit-transition: width 2s;
   -o-transition: width 2s;
-}
+} */
 </style>
