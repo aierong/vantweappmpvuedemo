29dedfc62e755457ab2cdadac2ae7494adc55a10
aierong <aierong@126.com>
2019-04-13 21:33:41 +0800

    =

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -7,27 +7,86 @@
 <template>
 
   <div>
-    Search 搜索
+
+    <!--
+    show-action	是否在搜索框右侧显示取消按钮
+    -->
+    <van-search :value="value1"
+                placeholder="请输入搜索关键词"
+                show-action
+                @search="onSearch"
+                @cancel="onCancel"/>
+
+    <mybr/>
+    <mybr/>
+    <van-search :value="value2"
+                placeholder="请输入搜索关键词"
+                use-action-slot
+                @change="onchange2"
+                @search="onSearch2">
+      <view slot="action"
+            @tap="onSearch3">搜索
+      </view>
+    </van-search>
   </div>
 
 </template>
 
 <!-- js脚本代码片段 -->
 <script>
+  import mybr from '@/components/mybr/mybr.vue'
+
   export default {
     name : "search" ,
+    components : {
+      mybr
+    } ,
     //数据模型
     data () {
       return {
-        msg : ''
+        value1 : '' ,
+        value2 : ''
       }
     } ,
     //方法
     methods : {
-      //methodsname() {
-      //代码搞这里
-      //},
+      onSearch ( event ) {
+        console.log( event )
 
+        //要搜索的值
+        let val = event.mp.detail;
+        console.log( val )
+
+        console.log( this.value1 )
+        // 最好 把模型同步一下
+        this.value1 = val;
+      } ,
+      //取消
+      onCancel ( event ) {
+        //这个参数好像没有啥用
+        console.log( event )
+      } ,
+      onSearch2 ( event ) {
+        console.log( event )
+
+        let val = event.mp.detail;
+        console.log( val )
+        this.value2 = val;
+      } ,
+      onchange2 ( event ) {
+        console.log( event )
+
+        let val = event.mp.detail;
+        console.log( val )
+
+        // 最好 把模型同步一下
+        this.value2 = val;
+      } ,
+      onSearch3 ( event ) {
+        console.log( event )
+
+        //要从模型this.value2中取值
+      } ,
     } ,
     //计算属性
     computed : {
