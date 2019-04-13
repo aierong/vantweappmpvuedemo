73a9525d3e6fb19ffa1f302c7c89c95fa3ef31e7
aierong <aierong@126.com>
2019-04-13 21:02:43 +0800

    =

--- src/pages/noticebar/noticebar.vue
+++ src/pages/noticebar/noticebar.vue
@@ -7,15 +7,26 @@
 <template>
 
   <div>
+    <van-notice-bar left-icon="https://img.yzcdn.cn/1.png"
+                    text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。"/>
 
+    <mybr/>
+    <mybr/>
+    <van-notice-bar mode="closeable"
+                    text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。"/>
   </div>
 
 </template>
 
 <!-- js脚本代码片段 -->
 <script>
+  import mybr from '@/components/mybr/mybr.vue'
+
   export default {
     name : "noticebar" ,
+    components : {
+      mybr
+    } ,
     //数据模型
     data () {
       return {
