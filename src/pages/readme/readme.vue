c15b711b40bef355c2f95789ae1ed036afe69f26
aierong <aierong@126.com>
2019-04-16 21:03:25 +0800

    1

--- src/pages/readme/readme.vue
+++ src/pages/readme/readme.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
     见本项目readme.md文件
+
+    <button open-type="share">分享我吧</button>
   </div>
 </template>
 
 <script>
 
+  import mybr from '@/components/mybr/mybr.vue'
 
   export default {
-
+    name : "search" ,
+    components : {
+      mybr
+    } ,
     data () {
       return {
         logs : [] ,
 
       }
     } ,
-
+    onShareAppMessage () {
+      return {
+        title : '猫叫助手|与猫咪交流的好帮手' ,
+        path : '/pages/index/main' ,
+        imageUrl : '/static/img/code.jpg'
+      }
+    } ,
     created () {
 
     }
