5d5ce11617ee5ad10ea9196e45dbe2e9f3eedf46
aierong <aierong@126.com>
2019-04-19 13:54:56 +0800

    1

--- CodeDemo/code/hellompvuevantweapp/src/App.vue
+++ CodeDemo/code/hellompvuevantweapp/src/App.vue
@@ -1,18 +1,17 @@
 <script>
 export default {
-  created () {
-    // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    console.log('app created and cache logs by setStorageSync')
-  }
+    // created () {
+    //   // 调用API从本地缓存中获取数据
+    //   const logs = wx.getStorageSync('logs') || []
+    //   logs.unshift(Date.now())
+    //   wx.setStorageSync('logs', logs)
+    //   console.log('app created and cache logs by setStorageSync')
+    // }
 }
 </script>
 
 <style>
-.container {
+/* .container {
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -20,7 +19,7 @@ export default {
   justify-content: space-between;
   padding: 200rpx 0;
   box-sizing: border-box;
-}
+} */
 /* this rule will be remove
 * {
   transition: width 2s;
