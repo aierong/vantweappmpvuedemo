7d92a4fc1c09b1b01874bb3753b3d23de6045f6d
aierong <aierong@126.com>
2019-05-05 10:51:52 +0800

    home

--- src/pages/actionsheet/actionsheet.vue
+++ src/pages/actionsheet/actionsheet.vue
@@ -9,9 +9,18 @@ Time: 10:33
 <template>
 
   <div>
+    <view>基础用法</view>
+    <van-button plain
+                @click="dj1"
+                type="primary">弹出actionsheet
+    </van-button>
+    <van-action-sheet :show="show1"
+                      :actions="actions1"
+                      @close="onClose1"
+                      @select="onSelect1"/>
     <mybr/>
     <mybr/>
-    actionsheetactionsheetactionsheet
+
   </div>
 
 </template>
@@ -28,15 +37,51 @@ Time: 10:33
     //数据模型
     data () {
       return {
-        msg : ''
+        show1 : false ,
+
+        //actions的格式，可以参考 https://youzan.github.io/vant-weapp/#/action-sheet
+        actions1 : [
+          {
+            name : '选项1'
+          } ,
+          {
+            name : '分享' ,
+            subname : '描述信息' ,
+            openType : 'share'
+          } ,
+          {
+            loading : true
+          } ,
+          {
+            name : '禁用选项' ,
+            disabled : true
+          }
+        ] ,
       }
     } ,
     //方法
     methods : {
-      //methodsname() {
-      //代码搞这里
-      //},
+      onClose1 () {
+        this.show1 = false;
+      } ,
+      onSelect1 ( event ) {
+        // console.log( event )
 
+        let obj = event.mp.detail;
+
+        // console.log( obj )
+
+        //取到选择的值
+        let name = obj.name;
+
+        console.log( name )
+
+        this.show1 = false;
+
+      } ,
+      dj1 () {
+        this.show1 = true;
+      } ,
     } ,
     //计算属性
     computed : {
