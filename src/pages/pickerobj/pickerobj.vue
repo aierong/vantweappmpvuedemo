4daa8725412477572a2ce3e58714562dd6a84b3f
aierong <aierong@126.com>
2019-05-05 21:37:29 +0800

    home

--- src/pages/pickerobj/pickerobj.vue
+++ src/pages/pickerobj/pickerobj.vue
@@ -9,9 +9,20 @@ Time: 21:17
 <template>
 
   <div>
+    <view>基础使用</view>
     <van-picker value-key="txt"
                 :columns="columnsobj1"
                 @change="onChange1"/>
+
+    <mybr/>
+    <mybr/>
+    <mybr/>
+    <mybr/>
+    <view>给默认值</view>
+    <van-picker value-key="txt"
+                :columns="colobjlist"
+                @change="onChange2"/>
+
   </div>
 
 </template>
@@ -48,6 +59,32 @@ Time: 21:17
             nos : "c"
           }
         ] ,
+
+        colobjlist : [
+          {
+            values : [
+              {
+                txt : "杭州" ,
+
+                ids : 11 ,
+                nos : "a1"
+              } ,
+              {
+                txt : "宁波" ,
+
+                ids : 22 ,
+                nos : "b2"
+              } ,
+              {
+                txt : "温州" ,
+
+                ids : 33 ,
+                nos : "c3"
+              }
+            ] ,
+            defaultIndex : 2
+          }
+        ] ,
       }
     } ,
     //方法
@@ -69,7 +106,23 @@ Time: 21:17
         console.log( val.ids )
         console.log( val.nos )
       } ,
+      onChange2 ( event ) {
+        console.log( event )
 
+        let obj = event.mp.detail;
+        console.log( obj )
+
+        //得到索引
+        let index = obj.index;
+
+        //得到值，是一个数组，取第一个对象
+        let val = obj.value[ 0 ];
+
+        // {txt: "宁波", ids: 2, nos: "b"}
+        console.log( val.txt )
+        console.log( val.ids )
+        console.log( val.nos )
+      } ,
     } ,
     //计算属性
     computed : {
