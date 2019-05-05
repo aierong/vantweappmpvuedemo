52ae6506e514355db9a68865ab6e2c897f450d90
aierong <aierong@126.com>
2019-05-05 10:08:00 +0800

    home

--- src/pages/stepper/stepper.vue
+++ src/pages/stepper/stepper.vue
@@ -9,11 +9,22 @@ Time: 9:58
 <template>
 
   <div>
+    <view>基础用法</view>
     <van-stepper :value="val1"
                  @change="onChange1"/>
     <mybr/>
     <mybr/>
-
+    <view>高级用法</view>
+    <!--
+    每次加减为1，可以对组件设置step、min、max属性
+    -->
+    <van-stepper :value="val2"
+                 integer
+                 min="5"
+                 max="40"
+                 step="2"
+                 @change="onChange2"
+    />
   </div>
 
 </template>
@@ -30,18 +41,23 @@ Time: 9:58
     //数据模型
     data () {
       return {
-        val1 : 11
+        val1 : 11 ,
+        val2 : 12
       }
     } ,
     //方法
     methods : {
       onChange1 ( event ) {
-        //
+
         console.log( event )
 
         this.val1 = event.mp.detail
       } ,
+      onChange2 ( event ) {
+        console.log( event )
 
+        this.val2 = event.mp.detail
+      }
     } ,
     //计算属性
     computed : {
