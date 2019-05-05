349710e15901803dbe2ab0b7de5342c4c220dcaf
aierong <aierong@126.com>
2019-05-05 11:11:55 +0800

    home

--- src/pages/actionsheet/actionsheet.vue
+++ src/pages/actionsheet/actionsheet.vue
@@ -20,7 +20,19 @@ Time: 10:33
                       @select="onSelect1"/>
     <mybr/>
     <mybr/>
-
+    <view>带标题的 ActionSheet</view>
+    <!--
+    如果传入了title属性，且不为空，则另外一种样式的ActionSheet，里面内容需要自定义
+    -->
+    <van-button plain
+                @click="dj2"
+                type="primary">弹出带标题的actionsheet
+    </van-button>
+    <van-action-sheet :show="show2"
+                      @close="onClose2"
+                      title="支持以下配送方式">
+      <view class="content">一些内容</view>
+    </van-action-sheet>
   </div>
 
 </template>
@@ -57,6 +69,7 @@ Time: 10:33
             disabled : true
           }
         ] ,
+        show2 : false ,
       }
     } ,
     //方法
@@ -66,7 +79,6 @@ Time: 10:33
       } ,
       onSelect1 ( event ) {
         // console.log( event )
-
         let obj = event.mp.detail;
 
         // console.log( obj )
@@ -81,6 +93,12 @@ Time: 10:33
       } ,
       dj1 () {
         this.show1 = true;
+      } ,
+      dj2 () {
+        this.show2 = true;
+      } ,
+      onClose2 () {
+        this.show2 = false;
       } ,
     } ,
     //计算属性
